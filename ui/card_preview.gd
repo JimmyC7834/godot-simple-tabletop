8b0cf11651046b2c03238584efa12a7ba3791c3b
jc4649 <jc4649@cs.washington.edu>
2024-03-12 04:47:39 -0700

    added minimize for card preview

--- ui/card_preview.gd
+++ ui/card_preview.gd
@@ -1,10 +1,20 @@
 extends Window
 
-@onready var texture_rect = $TextureRect
+@export var texture_rect: TextureRect
+@export var checkbox: CheckBox
 var registered: bool = false
+var size_y: int
 
 func _ready():
     close_requested.connect(hide)
+    
+    size_y = size.y
+    checkbox.pressed.connect(func():
+        if checkbox.button_pressed:
+            size = Vector2(size.x, size_y)
+        else:
+            size_y = size.y
+            size = Vector2(size.x, 25))
     
     visibility_changed.connect(func():
         if DragDropServer.cursor != null and !registered:
